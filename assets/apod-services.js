89283ff1ddc324fef4827f76e6bb1f80c9595c80
Jackson Gordon <gordon.jacksonh@gmail.com>
2018-08-14 09:39:08 -0600

    trying to synch

---
+++ assets/apod-services.js
@@ -0,0 +1,1 @@
+import Apod from "../models/Apod.js"
---
+++ models/Apod.js
@@ -0,0 +1,13 @@
+export default class Apod {
+  constructor(reqData) {
+    this.copyright = reqData.copyright
+    this.date = reqData.date
+    this.explanation = reqData.explanation
+    this.hdUrl = reqData.hdurl
+    this.mediaType = reqData.media_type
+    this.serviceVersion = reqData.service_version
+    this.title = reqData.title
+    this.url = reqData.url
+
+  }
+}
